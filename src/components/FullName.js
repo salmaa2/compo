1338e2ed646f822c2256b35e2aeb83459473ecf3
salma chebbi <salmaaa.shebby@gmail.com>
2022-01-13 19:55:20 +0100

    first

---
+++ src/components/FullName.js
@@ -0,0 +1,6 @@
+function Name() {
+
+    return (<h1 className='name'>my name is salma chebbi,</h1>);
+    
+}
+export default Name ;
---
+++ src/components/ProfilePhoto.js
@@ -0,0 +1,5 @@
+import image from './2.jpg'
+function photo(){
+    return (<img className='sou' src={image}/>)
+}
+export default photo ;
